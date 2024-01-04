7e86d4abf7dd6244427e54deecc50160e9210284
NikitaIluin <142826642+NikitaIluin@users.noreply.github.com>
2024-01-04 14:21:40 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,24 @@
-function checkForSpam(message) {
-  // Перетворюємо рядок на нижній регістр для порівняння
-  const lowerCaseMessage = message.toLowerCase();
-  
-  // Перевіряємо, чи містить рядок заборонені слова
-  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
-    return true;
-  } else {
-    return false;
-  }
+function getShippingCost(country) {
+    let shippingCost;
+
+    switch (country) {
+        case "China":
+            shippingCost = 100;
+            break;
+        case "Chile":
+            shippingCost = 250;
+            break;
+        case "Australia":
+            shippingCost = 170;
+            break;
+        case "Jamaica":
+            shippingCost = 120;
+            break;
+        default:
+            return "Sorry, there is no delivery to your country";
+    }
+
+    return `Shipping to ${country} will cost ${shippingCost} credits`;
 }
 
-// Перевірка функції
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-console.log(checkForSpam("[SPAM] lol i'm good")) // true 
-console.log(checkForSpam("Trade the new Rengoko Vampire"))// true
-console.log(checkForSpam("MAxi is a gay"))// false
- CSSAnimation()
+// Перев
